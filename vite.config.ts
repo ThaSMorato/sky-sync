35a5099a791fb148c157a35d31a9666485946c21
ThaSMorato <tha.s.morato@gmail.com>
2024-01-19 15:04:04 -0300

    chore: docker config and dayjs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tsConfigPaths()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  },
 })
